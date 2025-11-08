4a52018021be48be8189559f02fcdc6e56a42131
Lucas Sant <lucasfdesantana095@gmail.com>
2025-11-08 11:19:25 -0300

    feat: primeiros componentes criados

---
+++ portfolio-react/src/Redes.jsx
@@ -0,0 +1,14 @@
+function Redes(){ 
+
+    return  <>
+        <button>
+            Linkedin
+        </button>
+        <button>
+            Github
+        </button>
+    </>
+  
+}
+
+export default Redes
---
+++ portfolio-react/src/SectionOne.jsx
@@ -0,0 +1,12 @@
+function SectionOne(){
+    
+    return <>
+        <section>
+            <h1>Sobre Mim</h1>
+            <p>me chamo Lucas Ferreira</p>
+        </section>
+        </>
+        
+}
+
+export default SectionOne
