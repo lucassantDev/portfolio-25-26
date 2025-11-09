9d2e4365c1264fd07c14288ec1b4a8563e649a7d
Lucas Sant <lucasfdesantana095@gmail.com>
2025-11-09 11:16:31 -0300

    feat: estruturando portfolio

---
+++ portfolio-react/src/AboutMe.jsx
@@ -0,0 +1,20 @@
+
+
+function AboutMe(){
+    return(
+        <>
+        <section>
+            <div>
+                <h1>Sobre Mim</h1>
+                <p>
+                    Me chamo Lucas Santana, tenho 19 anos e atualmente sou estudante de Análise e Desenvolvimento de Sistemas na Faculdade Senac. <br/> Recentemente conclui o curso Técnico de Desenvolvimento de Sistemas pelo SENAI, o que me ajudou tanto na parte técnica quanto na parte social.
+                </p>
+            </div>
+        </section>
+        </>
+        
+    );
+            
+}
+
+export default AboutMe
---
+++ portfolio-react/src/Skills.jsx
@@ -0,0 +1,14 @@
+function Skills(){
+    return (
+        <>
+            <section className="">
+                <h1>Skilss</h1>
+                <div>React</div>
+                <div>Tailwindcss</div>
+                <div>Figma</div>
+            </section>
+        </>
+    )
+}
+
+export default Skills
