557eb26a32a84581f6a5a0754729b616fe0504fa
Lucas Sant <lucasfdesantana095@gmail.com>
2026-01-03 23:55:22 -0300

    feat: adicionando primeiro projeto no portfolio

--- portfolio-react/src/components/Projects.jsx
+++ portfolio-react/src/components/Projects.jsx
@@ -1,8 +1,14 @@
+import '../style/Projects.css'
+import listaDeCompras from '../assets/img/projeto-listaDeCompras.png'
+
 function Projects(){
     return(
         <>
-            <section>
-                <div></div>
+            <section className='section-projects'>
+                <div>
+                    <h2>Lista de compras</h2>
+                    <img src={listaDeCompras} alt="" />
+                </div>
                 <div></div>
                 <div></div>
             </section>
