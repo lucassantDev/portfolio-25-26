9c81efe2a8d019ba717fd819071a22a21b7bb742
Lucas Sant <lucasfdesantana095@gmail.com>
2026-01-03 23:56:11 -0300

    fix: correcao de bug useState

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -2,19 +2,20 @@
 import FirstSection from './components/FirstSection.jsx'
 import AboutMe from './components/AboutMe.jsx'
 import Skills from './components/Skills.jsx'
-// import Projects from './components/Projects.jsx'
+import Projects from './components/Projects.jsx'
 import Footer from './components/Footer.jsx'
 
 
 
 function App() {
-  // const [count, setCount] = useState(0)
+  // const [projetos, setProjetos] = useState('')
 
   return (
     <>
       <FirstSection/>
       <AboutMe/>
       <Skills/>
+      <Projects/>
       <Footer/>
     </>
   )
