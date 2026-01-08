fd2e52eedd97169d7191d2f24bcaf501cb330058
Lucas Sant <lucasfdesantana095@gmail.com>
2026-01-07 23:39:54 -0300

    feat: adicionando projeto agrosense

--- portfolio-react/src/components/Projects.jsx
+++ portfolio-react/src/components/Projects.jsx
@@ -8,9 +8,12 @@ function Projects(){
                 <h1>Projetos</h1>
                 <div className='div-projeto'>
                     <h2>Lista de compras</h2>
-                    <img src={listaDeCompras} alt="imagem do projeto lista de compras" className='projeto-01'/>
+                    <img src={listaDeCompras} alt="imagem do projeto lista de compras" className='projeto'/>
                 </div>
-                <div></div>
+                <div className='div-projeto'>
+                    <h2>Agrosense</h2>
+                    <img src="" alt="" className='projeto' />
+                </div>
                 <div></div>
             </section>
         
