3aa753f370c4840044df3cff5bc937e98bd3b2ab
Lucas Sant <lucasfdesantana095@gmail.com>
2026-01-15 22:47:44 -0300

    feat: criando campo para texto e links de projetos

--- portfolio-react/src/components/Projects.jsx
+++ portfolio-react/src/components/Projects.jsx
@@ -2,19 +2,35 @@ import '../style/Projects.css'
 import listaDeCompras from '../assets/img/projeto-listaDeCompras.png'
 import agrosense from '../assets/img/projeto-agrosense.png'
 
+import { useState } from 'react'
+
 function Projects(){
+
+
+    const [aboutProject, setAboutProject] = useState(false)
+
     return(
         <>
             <section className='section-projects'>
                 <h1>Projetos</h1>
-                <div className='div-projeto'>
-                    <h2>Lista de compras</h2>
-                    <img src={listaDeCompras} alt="imagem do projeto lista de compras" className='projeto'/>
+
+                <div className='div-project'>
+                    <img src={listaDeCompras} alt="imagem do projeto lista de compras" className='project'/>
+                    <div className='about-project'>
+                        <h2>Lista de compras</h2>
+                        <div className='arrow'></div>
+                    </div>
+
                 </div>
-                <div className='div-projeto'>
-                    <h2>Agrosense</h2>
-                    <img src={agrosense} alt="imagem do projeto agrosen" className='projeto' />
+
+                <div className='div-project'>
+                    <img src={agrosense} alt="imagem do projeto agrosen" className='project' />
+                    <div className='about-project'>
+                        <h2>Agrosense</h2>
+                        <div className='arrow'></div>
+                    </div>
                 </div>
+
                 <div></div>
             </section>
         
