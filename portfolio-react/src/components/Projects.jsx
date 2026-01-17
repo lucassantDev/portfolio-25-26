066f316390893f9b58a44994d660e132332b28e5
Lucas Sant <lucasfdesantana095@gmail.com>
2026-01-17 00:54:09 -0300

    feat: estruturando div de texto sobre projeto

--- portfolio-react/src/components/Projects.jsx
+++ portfolio-react/src/components/Projects.jsx
@@ -2,12 +2,11 @@ import '../style/Projects.css'
 import listaDeCompras from '../assets/img/projeto-listaDeCompras.png'
 import agrosense from '../assets/img/projeto-agrosense.png'
 
-import { useState } from 'react'
+// import { useState } from 'react'
 
 function Projects(){
 
-
-    const [aboutProject, setAboutProject] = useState(false)
+    
 
     return(
         <>
@@ -18,7 +17,11 @@ function Projects(){
                     <img src={listaDeCompras} alt="imagem do projeto lista de compras" className='project'/>
                     <div className='about-project'>
                         <h2>Lista de compras</h2>
-                        <div className='arrow'></div>
+                        <p>Projeto desenvolvido através de aulas do curso full-stack da Rocketseat, trabalhando <br /> principalmente com a manipulação do DOM.</p>
+                        <div className='button-acess-projects'>
+                            <button className="acess">repositório</button>
+                            <button className="acess">projeto</button>
+                        </div>
                     </div>
 
                 </div>
@@ -27,7 +30,11 @@ function Projects(){
                     <img src={agrosense} alt="imagem do projeto agrosen" className='project' />
                     <div className='about-project'>
                         <h2>Agrosense</h2>
-                        <div className='arrow'></div>
+                         <p>Projeto  React desenvolvido na faculdade, onde criei meus primeiros componentes, rotas com router e utilização da MUI</p>
+                        <div className='button-acess-projects'>
+                            <button className="acess">repositório</button>
+                            <button className="acess">projeto</button>
+                        </div>
                     </div>
                 </div>
 
