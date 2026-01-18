c7ec750287d6f036d04f088ae32c139b91b235ac
Lucas Sant <lucasfdesantana095@gmail.com>
2026-01-17 21:35:58 -0300

    feat: finalizando section de projetos

--- portfolio-react/src/components/Projects.jsx
+++ portfolio-react/src/components/Projects.jsx
@@ -6,7 +6,25 @@ import agrosense from '../assets/img/projeto-agrosense.png'
 
 function Projects(){
 
+    const repository = (nameRepository) =>{
+        if(nameRepository === 'agrosense'){
+            window.open('https://github.com/lucassantDev/agrosense-scripts', '_blank')
+        }
+
+        else if(nameRepository === 'lista'){
+            window.open('https://github.com/lucassantDev/lista-compras', '_blank')
+        }
+    } 
     
+    const project = (numberProject) =>{
+        if(numberProject === 0){
+            window.open('https://lucassantdev.github.io/lista-compras/', '_blank')
+        }
+
+        else if (numberProject === 1){
+            window.open('https://agrosenseweb.vercel.app/', '_blank')
+        }
+    }
 
     return(
         <>
@@ -19,8 +37,8 @@ function Projects(){
                         <h2>Lista de compras</h2>
                         <p>Projeto desenvolvido através de aulas do curso full-stack da Rocketseat, trabalhando <br /> principalmente com a manipulação do DOM.</p>
                         <div className='button-acess-projects'>
-                            <button className="acess">repositório</button>
-                            <button className="acess">projeto</button>
+                            <button className="acess" onClick={() => repository('lista')}>repositório</button>
+                            <button className="acess" onClick={() => project(0)}>projeto</button>
                         </div>
                     </div>
 
@@ -30,10 +48,10 @@ function Projects(){
                     <img src={agrosense} alt="imagem do projeto agrosen" className='project' />
                     <div className='about-project'>
                         <h2>Agrosense</h2>
-                         <p>Projeto  React desenvolvido na faculdade, onde criei meus primeiros componentes, rotas com router e utilização da MUI</p>
+                         <p>Projeto  React desenvolvido na faculdade, onde criei meus primeiros componentes, <br /> rotas com router e utilização da MUI</p>
                         <div className='button-acess-projects'>
-                            <button className="acess">repositório</button>
-                            <button className="acess">projeto</button>
+                            <button className="acess" name='agrosense' onClick={() => repository('agrosense')}>repositório</button>
+                            <button className="acess" onClick={() => project(1)}>projeto</button>
                         </div>
                     </div>
                 </div>
