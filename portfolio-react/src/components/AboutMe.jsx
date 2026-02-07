759f59423f1a9ecda34f6029d22ed2749b9d618d
Lucas Sant <lucasfdesantana095@gmail.com>
2026-02-06 22:39:12 -0300

    refactor: editando a secao sobre mim

--- portfolio-react/src/components/AboutMe.jsx
+++ portfolio-react/src/components/AboutMe.jsx
@@ -6,7 +6,7 @@ function AboutMe(){
         <section className='section-aboutme'>
                 <h1 className='aboutme-h1'>Sobre Mim</h1>
                 <p className='aboutme-p'>
-                    Me chamo Lucas Santana, tenho 20 anos, sou de Recife e atualmente estou no 2° período de Análise e Desenvolvimento de Sistemas na Faculdade Senac. <br/> <br/>
+                    Me chamo Lucas Santana, tenho 20 anos, sou de Recife e atualmente estou no 3° período de Análise e Desenvolvimento de Sistemas na Faculdade Senac. <br/> <br/>
 
                     Recentemente conclui o curso Técnico de Desenvolvimento de Sistemas pelo SENAI, o que me ajudou tanto na parte técnica quanto na parte social. <br/> <br/>
 
