01c0c0f90535e31bfc5d5418f1139d85ad948b85
Lucas Sant <lucasfdesantana095@gmail.com>
2026-02-10 23:04:13 -0300

    refactor: adicionando map para estruturar projetos

--- portfolio-react/src/components/Projects.jsx
+++ portfolio-react/src/components/Projects.jsx
@@ -1,43 +1,55 @@
-import '../style/Projects.css'
-import listaDeCompras from '../assets/img/projeto-listaDeCompras.png'
-import agrosense from '../assets/img/projeto-agrosense.png'
 
-// import { useState } from 'react'
+import '../style/Projects.css'
+
+
+import { useState } from 'react'
 
 function Projects(){
 
-    const repository = (nameRepository) =>{
-        if(nameRepository === 'agrosense'){
-            window.open('https://github.com/lucassantDev/agrosense-scripts', '_blank')
+     const [projects] = useState([
+        {
+        id: 1,
+        title: "Agrosense",
+        image: "../src/assets/img/projeto-agrosense.png",
+        link: "https://agrosenseweb.vercel.app/",
+        repository: "https://github.com/lucassantDev/agrosense-scripts",
+        description: "Trabalho Agrosense desenvolvido para o projeto integrador do 2° semestre da Faculdade Senac"
+        },
+        {
+        id: 2,
+        title: "Lista de compras",
+        image: "../src/assets/img/projeto-listaDeCompras.png",
+        link: "https://lucassantdev.github.io/lista-compras/",
+        repository: "https://github.com/lucassantDev/lista-compras", 
+        description: "Projeto desenvolvido através do curso da Rocketseat Full-Stack"
         }
-
-        else if(nameRepository === 'lista'){
-            window.open('https://github.com/lucassantDev/lista-compras', '_blank')
-        }
-    } 
     
-    const project = (numberProject) =>{
-        if(numberProject === 0){
-            window.open('https://lucassantdev.github.io/lista-compras/', '_blank')
-        }
+    
+    ])
 
-        else if (numberProject === 1){
-            window.open('https://agrosenseweb.vercel.app/', '_blank')
-        }
-    }
 
     return(
-        <>
-            <section className='section-projects'>
-                <h1>Projetos</h1>
+    <>
+        <section className='section-projects'>
+            <h1>Projetos</h1>
 
-                {/* refatorando */}
+                {projects.map(project => (
+                    // <h2>{project.title}</h2>
+                    <div className='displayMap'>
+                        <img src={project.image} alt="imagem referente ao projeto" className='imageMap' />
 
-                <div></div>
-            </section>
+                        <div className='button-acess-projects'>
+                            <a href={project.repository} target='_blank'>Repositório</a>
+                            <a href={project.link} target='_blank'>Projeto</a>
+                            
+                        </div>
+                    </div> 
+                ))}
+        </section>
+
+    </>
         
-        </>
-    )
+)
 }
 
-export default Projects
+export default Projects;
