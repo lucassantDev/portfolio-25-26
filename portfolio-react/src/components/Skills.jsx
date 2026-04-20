99257d8b2c6faee2337bef9e6384cef787df5a8a
Lucas Sant <lucasfdesantana095@gmail.com>
2026-04-20 01:09:24 -0300

    refactor: reestruturando componente de skills

--- portfolio-react/src/components/Skills.jsx
+++ portfolio-react/src/components/Skills.jsx
@@ -8,41 +8,16 @@ import { FaJsSquare } from "react-icons/fa";
 import { SiTailwindcss } from "react-icons/si";
 
 
-
 function Skills(){
     return (
         <>
             <section className="section-skills">
                 <h1>Skills</h1>
                 <div className='div-stack '>
-                    <div className='stack'>
-                        <FaJsSquare class='icon'/>
-                        Javascript
-                    </div>
-                    <div className='stack'>
-                        <FaReact class='icon'/>
-                        <p>React</p>
-                    </div>
-                    <div className='stack'>
-                        <RiPhpFill class='icon'/>
-                        <p>PHP</p>
-                    </div>
-                    <div className='stack'>
-                        <SiTailwindcss class='icon'/>
-                        Tailwindcss
-                    </div>
-                    <div className='stack'>
-                        <FiFigma class='icon'/>
-                        Figma
-                    </div>
-                    <div className='stack'>
-                        <SiN8N class='icon'/>
-                        N8N
-                    </div>
                 </div>
             </section>
         </>
     )
 }
 
-export default Skills
+export default Skills;
